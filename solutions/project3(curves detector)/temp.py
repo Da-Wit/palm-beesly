80e9fd606224121fc98b2de3e98a357f5b662a81
David Jung <thinkdifferent141592653@gmail.com>
2022-01-27 22:44:05 +0900

    feature cv2.ximgprock.thinning

---
+++ solutions/project3(curves detector)/temp.py
@@ -0,0 +1,9 @@
+import cv2
+
+path = "C:/Users/think/Desktop/temp/vert_thresh.png"
+img = cv2.imread(path)
+img = cv2.cvtColor(img,cv2.COLOR_RGB2GRAY)
+cv2.imshow("ori", img)
+rst = cv2.ximgproc.thinning(img)
+cv2.imshow("after", rst)
+cv2.waitKey(0)
