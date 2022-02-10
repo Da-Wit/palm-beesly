831ee4694a3d67df092aeacb4b03a2db252eae7b
David Jung <different141592653@gmail.com>
2022-02-11 06:18:44 +0900

    refactor: merge temp_ori_only_function and execute_second_part into __name__ == "__main__", separate init part by adding init_imgs

--- solutions/project3(curves detector)/main.py
+++ solutions/project3(curves detector)/main.py
@@ -99,51 +99,6 @@ def find_one_orientation_lines(img_param, min_grayscale, max_line_distance, is_h
     return lines
 
 
-# 이미지와 값 조정 변수를 넣어주면 최종적으로 시각화된 이미지를 가로, 세로로 나눠 리턴함
-# 외부에서 최종적으로 사용할 함수
-# TODO change function name
-def temp_ori_only_function(img_param, min_grayscale, min_line_length, max_line_distance,
-                           number_of_lines_to_leave, flattening_distance):
-    copied = copy.deepcopy(img_param)
-    height, width = copied.shape[:2]
-    horizontal_img = np.zeros((height, width, 1), dtype=np.uint8)
-
-    # Finding lines part
-    horizontal_lines = find_one_orientation_lines(copied, min_grayscale, max_line_distance,
-                                                  is_horizontal=True)
-
-    # Visualizing part
-    horizontal_img = horizontal_lines.visualize_lines(horizontal_img, color=True)
-
-    cv2.imshow("hori before combining", horizontal_img)
-
-    combine_max_distance = 4
-    horizontal_lines.combine(combine_max_distance)
-
-    # Filtering part
-    horizontal_lines.filter_by_line_length(min_line_length)
-
-    # Leaving long lines part
-    horizontal_lines.leave_long_lines(number_of_lines_to_leave)
-
-    # cv2.imshow("hori_before_flattening", hori_before_flattening)
-
-    # Flattening part
-    # horizontal_lines.flatten(flattening_distance, is_horizontal=True)
-
-    # Visualizing part
-    horizontal_img = horizontal_lines.visualize_lines(horizontal_img, color=True)
-
-    cv2.imshow("hori after combining", horizontal_img)
-
-    hori_gray = cv2.cvtColor(horizontal_img, cv2.COLOR_BGR2GRAY)
-    _, hori_thresh = cv2.threshold(hori_gray, 1, 255, cv2.THRESH_BINARY)
-    thinned_hori = cv2.ximgproc.thinning(
-        hori_thresh)  # to use this function paste it: pip install opencv-contrib-python
-
-    return thinned_hori
-
-
 def get_bgr_min_max(original_img, lines, vertices):
     copied = copy.deepcopy(original_img)
 
@@ -177,41 +132,7 @@ def get_bgr_min_max(original_img, lines, vertices):
     return [[b_min, b_max], [g_min, g_max], [r_min, r_max]]
 
 
-def execute_second_part(thinned, original_img, min_grayscale, max_line_distance, vertices):
-    copied = copy.deepcopy(thinned)
-    height, width = copied.shape[:2]
-    horizontal_img = np.zeros((height, width, 1), dtype=np.uint8)
-
-    # Finding lines part
-    horizontal_lines = find_one_orientation_lines(copied, min_grayscale, max_line_distance,
-                                                  is_horizontal=True)
-
-    combine_max_distance = 4
-    horizontal_lines.combine(combine_max_distance)
-
-    # Visualizing part
-    horizontal_img = horizontal_lines.visualize_lines(horizontal_img, color=True)
-    b_minmax, g_minmax, r_minmax = get_bgr_min_max(original_img, horizontal_lines, vertices)
-    print(b_minmax, g_minmax, r_minmax)
-
-    b_minmax[0] = round(b_minmax[0] * 0.9)
-    b_minmax[1] = round(b_minmax[0] * 1.1)
-    g_minmax[0] = round(g_minmax[0] * 0.9)
-    g_minmax[1] = round(g_minmax[0] * 1.1)
-    r_minmax[0] = round(r_minmax[0] * 0.9)
-    r_minmax[1] = round(r_minmax[0] * 1.1)
-
-    second_min_grayscale = 30
-    # horizontal_lines = find_one_orientation_lines(copied, min_grayscale, max_line_distance,
-    #                                               is_horizontal=True)
-    #
-    # combine_max_distance = 4
-    # horizontal_lines.combine(combine_max_distance)
-
-    return horizontal_img
-
-
-if __name__ == "__main__":
+def init_imgs():
     original_img_path = "/Users/david/workspace/palm-beesly/test_img/sample5.png"
     original_img = cv2.imread(original_img_path)
     image_path = "/Users/david/workspace/palm-beesly/test_img/sample5.4.png"
@@ -224,32 +145,55 @@ if __name__ == "__main__":
     if img is None:
         print("Image is empty!!")
         exit(1)
+    return original_img, img
 
+
+# 이미지와 값 조정 변수를 넣어주면 최종적으로 시각화된 이미지를 가로, 세로로 나눠 리턴함
+# 외부에서 최종적으로 사용할 함수
+if __name__ == "__main__":
     start = timeit.default_timer()
 
+    original_img, img = init_imgs()
     img, vertices = get_roi(img)
 
     min_grayscale = 70  # Default value is 63
     max_grayscale = 200
-
-    # The minimum number of dots in one line
-    min_line_length = 4  # Default value is 4
+    min_line_length = 4  # The minimum number of dots in one line, Default value is 4
     max_line_distance = 5  # Default value is 3
     number_of_lines_to_leave = 10  # Default value is 10
     flattening_distance = 4  # Default value is 4
 
-    rst = temp_ori_only_function(img,
-                                 min_grayscale,
-                                 min_line_length,
-                                 max_line_distance,
-                                 number_of_lines_to_leave,
-                                 flattening_distance)
+    height, width = img.shape[:2]
+    horizontal_img = np.zeros((height, width, 1), dtype=np.uint8)
 
-    B = execute_second_part(rst, original_img, min_grayscale, max_line_distance, vertices)
+    horizontal_lines = find_one_orientation_lines(img, min_grayscale, max_line_distance, is_horizontal=True)
+    rendered_lines = horizontal_lines.visualize_lines(horizontal_img, color=True)
+    cv2.imshow("before combining", rendered_lines)
+
+    horizontal_lines.combine(max_distance=4)
+    horizontal_lines.filter_by_line_length(min_line_length)
+    horizontal_lines.leave_long_lines(number_of_lines_to_leave)
+    rendered_filtered = horizontal_lines.visualize_lines(rendered_lines, color=True)
+    cv2.imshow("after combining", rendered_filtered)
+
+    grayscale = cv2.cvtColor(rendered_filtered, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(grayscale, 1, 255, cv2.THRESH_BINARY)
+    thinned = cv2.ximgproc.thinning(thresh)  # to use this function paste it: pip install opencv-contrib-python
+
+    horizontal_lines = find_one_orientation_lines(thinned, min_grayscale, max_line_distance, is_horizontal=True)
+    horizontal_lines.combine(max_distance=4)
+    b_minmax, g_minmax, r_minmax = get_bgr_min_max(original_img, horizontal_lines, vertices)
+    print(b_minmax, g_minmax, r_minmax)
+
+    b_minmax[0] = round(b_minmax[0] * 0.9)
+    b_minmax[1] = round(b_minmax[0] * 1.1)
+    g_minmax[0] = round(g_minmax[0] * 0.9)
+    g_minmax[1] = round(g_minmax[0] * 1.1)
+    r_minmax[0] = round(r_minmax[0] * 0.9)
+    r_minmax[1] = round(r_minmax[0] * 1.1)
+
     cv2.imshow("original", img)
-
-    cv2.imshow("hori", rst)
-    cv2.imshow("second", B)
+    cv2.imshow("thinned", thinned)
 
     stop = timeit.default_timer()
     print(round(stop - start, 6))
