bb69b440cbf801742827069ec66de96eef1ae634
David Jung <different141592653@gmail.com>
2022-02-13 05:34:08 +0900

    feat: fix a result error, rollback filter_by_line_length, add debugging code, remove combine function

--- solutions/project3(curves detector)/lines.py
+++ solutions/project3(curves detector)/lines.py
@@ -32,13 +32,14 @@ class Lines:
             lineOne = self.line_list[idx]
             point_list = copy.deepcopy(lineOne.all_point_list)
             new_lineOne.all_point_list += point_list
+            new_lineOne.point_list_in_work_area += point_list
 
         for index in range(len(indices) - 1, -1, -1):
             del self.line_list[indices[index]]
 
         self.add_line(new_lineOne)
 
-    def handle_point(self, point, max_distance, unique_num):
+    def handle_point(self, point, max_distance, unique_num, is_horizontal, debug=False, img_for_debugging=None):
         list_of_index_of_close_lines = self.get_index_list_of_close_lines(point, max_distance)
         number_of_close_lines = len(list_of_index_of_close_lines)
 
@@ -46,27 +47,37 @@ class Lines:
         # 새로운 hline 만들어서 nline에 append
         # 즉, 새로운 선 발견했다고 추정해 새로운 선 추가
         if number_of_close_lines == 0:
-            lineOne = LineOne(self.number_of_front_points_to_find_slope, unique_num)
-            lineOne.add_point(point)
-            self.add_line(lineOne)
+            new_lineOne = LineOne(self.number_of_front_points_to_find_slope, unique_num)
+            new_lineOne.add_point(point)
+            self.add_line(new_lineOne)
 
         # 점 주변에 선이 1개일 때
         # 그 1개의 선에 점 추가
         elif number_of_close_lines == 1:
             index = list_of_index_of_close_lines[0]
-            lineOne = self.line_list[index]
-            lineOne.add_point(point)
+            new_lineOne = self.line_list[index]
+            new_lineOne.add_point(point)
 
         # 점 주변에 선이 1개보다 많을 때
         else:
             new_lineOne = LineOne(self.number_of_front_points_to_find_slope, unique_num)
+            new_lineOne.add_point(point)
             self.combine_only_params(list_of_index_of_close_lines, new_lineOne)
+            new_lineOne.renew_work_area(point, max_distance, is_horizontal)
+
+        if debug:
+            print(f"number_of_close_lines : {number_of_close_lines}")
+            img_for_debugging *= 0
+            for lineOne in self.line_list:
+                for point in lineOne.all_point_list:
+                    x, y = point
+                    img_for_debugging[y][x] = copy.deepcopy(lineOne.color)
 
     def filter_by_line_length(self, min_length):
-        self.line_list = []
-        for lineOne in self.line_list:
-            if len(lineOne.all_point_list) >= min_length:
-                self.line_list.append(lineOne)
+        self.line_list = [
+            lineOne for lineOne in self.line_list \
+            if len(lineOne.all_point_list) >= min_length
+        ]
 
     def visualize_lines(self, height, width):
         copied_img = np.zeros((height, width, 1), dtype=np.uint8)
@@ -105,87 +116,9 @@ class Lines:
         for point1 in lineOne1.all_point_list:
             for point2 in lineOne2.all_point_list:
                 distance = utils.distance_between(point1, point2)
-                if distance < max_distance:
+                if distance <= max_distance:
                     return True
         return False
-
-    # TODO change this stupid function's and variables' name
-    def pre_combine(self, max_distance):
-        new_line_list = []
-        line_list = copy.deepcopy(self.line_list)
-
-        for lineOne1 in line_list:
-            new_unique_num = lineOne1.unique_num
-            recursive_same_indices = []
-            for lineOne2 in new_line_list:
-                if self.is_connectable(lineOne1, lineOne2, max_distance):
-                    same_indices = utils.find_indices(new_line_list,
-                                                      lambda index, value:
-                                                      value.unique_num == lineOne2.unique_num)
-                    recursive_same_indices += same_indices
-
-            for index in recursive_same_indices:
-                new_line_list[index].set_unique_num_to(new_unique_num)
-
-            new_line_list.append(lineOne1)
-        return new_line_list
-        # for i in range(len(line_list)):
-        #     for j in range(len(new_line_list)):
-        #         if self.is_connectable(self.line_list[i]['hline'], new_line_list[j]['hline'], min_distance):
-        #             index_list_of_same_index = utils.find_indices(new_line_list,
-        #                                                           lambda index, value: value['index'] ==
-        #                                                                                new_line_list[j][
-        #                                                                                    'index'])
-        #
-        #             for index_of_same_index in index_list_of_same_index:
-        #                 new_line_list[index_of_same_index]['index'] = self.line_list[i]['index']
-        #             # new_line_list[j]['index'] = self.line_list[i]['index']
-
-        # a = 6
-        # a1 = 0
-        # a1 = round(a * 0.25)
-        # a1 = round(a * 0.5)
-        # a1 = round(a * 0.75)
-        # a1 = a
-        #
-        # i = 0
-        # while True:
-        #     lineOne = line_list[i]
-        #     line_len = len(lineOne.all_point_list)
-        #
-        #     first_idx = 0
-        #     one_fourth_idx = round(line_len * (1 / 4)) - 1
-        #     middle_idx = round(line_len * (1 / 2)) - 1
-        #     three_fourth_idx = round(line_len * (3 / 4)) - 1
-        #     last_idx = line_len - 1
-        #     indices = list({first_idx, one_fourth_idx, middle_idx, three_fourth_idx, last_idx})
-
-    def combine_absolutely(self, new_line_list):
-        line_list = copy.deepcopy(new_line_list)
-        unique_nums = []
-        combined_absolutely = []
-        for lineOne in line_list:
-            if unique_nums.count(lineOne.unique_num) == 0:
-                unique_nums.append(lineOne.unique_num)
-                combined_absolutely.append(lineOne)
-            else:
-                index = unique_nums.index(lineOne.unique_num)
-                combined_absolutely[index].all_point_list.extend(lineOne.all_point_list)
-
-        # for i in range(len(new_line_list)):
-        #     if unique_nums.count(new_line_list[i].unique_num) == 0:
-        #         unique_nums.append(new_line_list[i].unique_num)
-        #         combined_absolutely.append([])
-        # for i in range(len(new_line_list)):
-        #     index = unique_nums.index(new_line_list[i]['index'])
-        #     combined_absolutely[index].extend(new_line_list[i]['hline'])
-        return combined_absolutely
-
-    # TODO change variable name max_distance to something another
-    def combine(self, max_distance):
-        pre_combined = self.pre_combine(max_distance)
-        absolutely_combined = self.combine_absolutely(pre_combined)
-        self.line_list = absolutely_combined
 
     def imshow(self, title, height, width):
         img = self.visualize_lines(height, width)
--- solutions/project3(curves detector)/main.py
+++ solutions/project3(curves detector)/main.py
@@ -6,8 +6,8 @@ import timeit
 import numpy as np
 
 
-def get_roi(img, min_grayscale=0):
-    copied = copy.deepcopy(img)
+def get_roi(img_param, min_grayscale=0):
+    copied = copy.deepcopy(img_param)
     height, width = copied.shape[:2]
 
     topmost = 0
@@ -59,9 +59,85 @@ def get_roi(img, min_grayscale=0):
                                                                       'leftmost': leftmost, 'rightmost': rightmost}
 
 
-def adaptive_threshold(img):
-    result = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 0)
+def adaptive_threshold(img_param):
+    result = cv.adaptiveThreshold(img_param, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 0)
     return result
+
+
+# Now it's same with find_one_orientation_lines
+# Please update this function too, when updating function find_one_orientation_lines
+def find_one_orientation_lines_with_debugging(img_param, max_line_distance, is_horizontal):
+    height, width = img_param.shape[:2]
+    img_for_debugging = np.zeros((height, width, 1), dtype=np.uint8)
+    img_for_debugging = cv.cvtColor(img_for_debugging, cv.COLOR_GRAY2BGR)
+
+    if is_horizontal:
+        first_for_loop_max = width
+        second_for_loop_max = height
+    else:
+        first_for_loop_max = height
+        second_for_loop_max = width
+
+    lines = Lines()
+    unique_num = 0
+    skipped = False
+    num_of_skip = 40
+
+    for i in range(first_for_loop_max):
+        if is_horizontal:
+            x = i
+        else:
+            y = i
+
+        for j in range(second_for_loop_max):
+            if skipped:
+                num_of_skip -= 1
+
+            if is_horizontal:
+                y = j
+            else:
+                x = j
+
+            if skipped:
+                if img_param[y][x] == 255:
+                    lines.handle_point([x, y], max_line_distance, unique_num, is_horizontal, debug=True,
+                                       img_for_debugging=img_for_debugging)
+                    unique_num += 1
+                if num_of_skip == 0:
+                    skipped = False
+                    img_for_debugging *= 0
+                    for lineOne in lines.line_list:
+                        for point in lineOne.all_point_list:
+                            x2, y2 = point
+                            img_for_debugging[y2][x2] = lineOne.color
+            else:
+                if img_param[y][x] == 255:
+                    lines.handle_point([x, y], max_line_distance, unique_num, is_horizontal, debug=True,
+                                       img_for_debugging=img_for_debugging)
+                    unique_num += 1
+                prev_bgr = copy.deepcopy(img_for_debugging[y][x])
+                img_for_debugging[y][x] = [0, 0, 255]
+                cv.imshow("debugging", img_for_debugging)
+                k = cv2.waitKey(0)
+                # for문 도중 Esc를 누르면 프로그램이 종료되게 함
+                if k == 113:  # q key to stop
+                    exit(0)
+                if k == 115:  # s key to skip
+                    skipped = True
+                    num_of_skip = 20
+                    img_for_debugging[y][x] = prev_bgr
+                else:
+                    img_for_debugging[y][x] = prev_bgr
+
+        if is_horizontal:
+            x = i
+            y = 0
+        else:
+            x = 0
+            y = i
+        lines.renew_work_area([x, y], max_line_distance, is_horizontal)
+
+    return lines
 
 
 # 한 방향으로의 hline들의 리스트, nline을 리턴함
@@ -78,16 +154,18 @@ def find_one_orientation_lines(img_param, max_line_distance, is_horizontal):
     lines = Lines()
     unique_num = 0
     for i in range(first_for_loop_max):
+        if is_horizontal:
+            x = i
+        else:
+            y = i
         for j in range(second_for_loop_max):
             if is_horizontal:
-                x = i
                 y = j
             else:
                 x = j
-                y = i
 
             if img_param[y][x] == 255:
-                lines.handle_point([x, y], max_line_distance, unique_num)
+                lines.handle_point([x, y], max_line_distance, unique_num, is_horizontal)
                 unique_num += 1
         if is_horizontal:
             x = i
@@ -101,13 +179,13 @@ def find_one_orientation_lines(img_param, max_line_distance, is_horizontal):
 
 
 def init_imgs():
-    image_path = "/Users/david/workspace/palm-beesly/test_img/sample1.4.png"
-    img = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
+    image_path = "/Users/david/workspace/palm-beesly/test_img/sample5.4.png"
+    image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
 
-    if img is None:
+    if image is None:
         print("Image is empty!!")
         exit(1)
-    return img
+    return image
 
 
 def get_thinned(bgr_img):
@@ -120,44 +198,41 @@ def get_thinned(bgr_img):
 if __name__ == "__main__":
     grayed = init_imgs()
     img, vertices = get_roi(grayed)
-
     thr = adaptive_threshold(img)
+
+    cv.imshow("original", img)
     cv.imshow("adaptiveThreshold", thr)
 
-    max_line_distance = 5  # Default value is 3
-    min_line_length = 4  # The minimum number of dots in one line, Default value is 4
-    number_of_lines_to_leave = 10  # Default value is 10
+    max_line_distance = 2  # Default value is 3
+    combining_distance = 1
+    min_line_length = 100  # The minimum number of dots in one line, Default value is 4
+    number_of_lines_to_leave = 6  # Default value is 10
 
     height, width = thr.shape[:2]
-    horizontal_img = np.zeros((height, width, 1), dtype=np.uint8)
 
     start = timeit.default_timer()
-    lines = find_one_orientation_lines(thr, max_line_distance, is_horizontal=True)
+    # lines = find_one_orientation_lines_with_debugging(thr, max_line_distance, is_horizontal=False)
+    lines = find_one_orientation_lines(thr, max_line_distance, is_horizontal=False)
     lines.imshow("just found", height, width)
     stop = timeit.default_timer()
     print("Finding part", round(stop - start, 6))
 
     start = timeit.default_timer()
-    lines.combine(max_distance=1)
-    lines.imshow("combined", height, width)
-    stop = timeit.default_timer()
-    print("combining part", round(stop - start, 6))
-
-    start = timeit.default_timer()
-    lines.filter_by_line_length(min_line_length)
+    # lines.filter_by_line_length(min_line_length)
+    lines.sort()
+    del lines.line_list[0]
     lines.leave_long_lines(number_of_lines_to_leave)
     filtered = lines.visualize_lines(height, width)
     cv.imshow("filtered", filtered)
     stop = timeit.default_timer()
     print("Filtering part", round(stop - start, 6))
 
-    start = timeit.default_timer()
-    thinned = get_thinned(filtered)
-    cv.imshow("thinned", thinned)
-    stop = timeit.default_timer()
-    print("Thinning part", round(stop - start, 6))
+    # start = timeit.default_timer()
+    # thinned = get_thinned(filtered)
+    # cv.imshow("thinned", thinned)
+    # stop = timeit.default_timer()
+    # print("Thinning part", round(stop - start, 6))
 
-    cv.imshow("original", img)
     print("Done")
 
     cv.waitKey(0)
